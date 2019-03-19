102770de0809dd15d3d3575be6cb8fdbdf207827
SkanderHannachi <skander.hannachi@esprit.tn>
2019-03-19 19:44:07 +0100

    testing sine sig as cntrl for frequency modulation effect

--- DigitalEcho.ino
+++ DigitalEcho.ino
@@ -6,7 +6,7 @@
 
 static const uint32_t LEN = 0x06400;
 const float THRTH = .0002; 
-const int TIME_RAMP = 150; 
+const int TIME_RAMP = 350; 
 const int myInput = AUDIO_INPUT_LINEIN;
 const int NUM_READINGS = 100; 
 
@@ -20,28 +20,32 @@ float old_time = 200;
 float err = 0; 
 
 
-float compute_time(int val);
+float compute_average(int val);
 
 AudioControlSGTL5000	 audioShield;
-AudioSynthWaveformDc	     time_delta_sig;           
+AudioSynthWaveformDc	     time_delta_sig;    
+AudioSynthWaveformSine	 sine_sig;
 AudioEffectModDelay	 delay2;
 AudioInputI2S  	  audioInput;         // audio shield: mic or line-in
 AudioOutputI2S 	  audioOutput;        // audio shield: headphones & 
 AudioConnection 		patchCord1(audioInput, 0, delay2, 0);
 AudioConnection 		patchCord2(delay2, 0, audioOutput, 0);
 AudioConnection 		patchCord100(time_delta_sig,0, delay2, 1);
+// AudioConnection 		patchCord100(sine_sig,0, delay2, 1);
 
 
 
 void setup() {
-	AudioMemory(10);
-
+	AudioMemory(16);
 	audioShield.enable();
 	audioShield.inputSelect(myInput);
 	audioShield.volume(100);
-// 	Serial.begin(9600); 
+	audioShield.adcHighPassFilterDisable();
+ 	Serial.begin(9600); 
 	delay2.setbuf(LEN, delaybuf);
 	time_delta_sig.amplitude(1);
+// 	sine_sig.amplitude(1); 
+// 	sine_sig.frequency(10);
 	AudioProcessorUsageMaxReset();
 	AudioMemoryUsageMaxReset();
 
@@ -49,17 +53,17 @@ void setup() {
 
 void loop() {
 	int knob_time = analogRead(3)/ 3 + 5;
-// 	Serial.println(deltime);
-	deltime = compute_time(knob_time)/340; 
+	Serial.println(deltime);
+	deltime = compute_average(knob_time)/340; 
 	err = abs(old_time - deltime) ; 
 	if (err > THRTH)  time_delta_sig.amplitude(.02+deltime, TIME_RAMP);
+// 	if (err > THRTH)  sine_sig.frequency(100*deltime);
 	old_time = deltime; 
 	delay(1);
 }
 
 
-float compute_time(int val){ 
-
+float compute_average(int val){ 
 	total = total - readings[readIndex];
 	readings[readIndex] = val;
 	total = total + readings[readIndex];
